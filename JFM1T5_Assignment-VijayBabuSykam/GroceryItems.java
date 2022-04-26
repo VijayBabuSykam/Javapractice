c082b1ab5ad2aa2d62ae2c1272d16bb920a58450
Vijay Babu Sykam <92039985+VijayBabuSykam@users.noreply.github.com>
2022-04-26 12:30:13 +0530

    Add files via upload

---
+++ JFM1T5_Assignment-VijayBabuSykam/GroceryItems.java
@@ -0,0 +1,48 @@
+/* JFM1T5_Assignment 2:
+
+   Write a program to store your 10 different expenses done in this month and print the highest price you incurred.
+   Prompt the user for the 10 numbers to be input from the terminal.
+   Sample Input:
+   45.90
+   23.56
+   123
+   23
+   90.00
+   344
+   44
+   60.01
+   267
+   100
+   
+   Expected Output:
+
+   The highest price you incurred is 344.0
+
+*/
+
+import java.util.Scanner;
+
+public class GroceryItems {
+
+//Define the main method
+public static void main(String args[]){
+//Declare and initialize variables
+float a[]=new float[10];
+  Scanner sc=new Scanner(System.in);
+//Take the 10 different expenses price
+ System.out.println("Input values are");
+//Check the 10 different expenses price using for loop
+for(int i=0;i<10;i++){
+  a[i]=sc.nextFloat();
+}
+//Calculate the highest price in all expenses and check if the expenses is more than new item. If true then return max.
+float max=a[0];
+  for(int i=0;i<10;i++){
+    if(a[i]>max){
+      max=a[i];
+    }
+  }
+//Print the result
+System.out.println("highest price incurred is :" +max);
+}
+}
---
+++ JFM1T5_Assignment-VijayBabuSykam/TransposeMatrix.java
@@ -0,0 +1,74 @@
+/* JFM1T5_Assignment 4:
+
+   Write a program to create a square matrix and print its transpose.
+   Prompt the user input from the terminal.
+   
+  Sample Input:
+  Enter number of rows: 3
+  Enter number of columns: 3
+  Elements  [1,1] : 2
+  Elements  [1,2] : 3
+  Elements  [1,3] : 4
+  Elements  [2,1] : 1
+  Elements  [2,2] : 2
+  Elements  [2,3] : 3
+  Elements  [3,1] : 4
+  Elements  [3,2] : 5
+  Elements  [3,3] : 6
+   
+  Expected Output:
+    
+  After Transpose Matrix  
+  2       1       4
+  3       2       5
+  4       3       6
+   
+*/
+
+import java.util.Scanner;
+class TransposeMatrix{
+  //Define the main method
+  public static void main(String[] args){
+    //Declare the variables
+    int i,j,row,col;
+   //Take input from user to enter rows and column values
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the row value");
+    row = sc.nextInt();
+    System.out.println("Enter the col value");
+    col=sc.nextInt();
+     int a[][] = new int[row][col];
+   
+    for(i=0;i<row;i++)
+      {
+        for(j=0;j<col;j++)
+          {
+            System.out.print("element ["+(i+1)+"],["+(j+1)+"] :");
+            a[i][j]=sc.nextInt();
+          }
+      }
+    System.out.println(" before transpose matrix :");
+    for(i=0;i<row;i++)
+      {
+        for(j=0;j<col;j++)
+          {
+            System.out.print(a[i][j] + "  ");
+          }
+            System.out.println( );
+      }
+   //Print the transpose matrix
+   System.out.println(" after transpose matrix :");
+    //Convert the square matrix into transpose
+    for(i=0;i<col;i++)
+      {
+        for(j=0;j<row;j++)
+          {
+    //Printing j and i instead of i and j
+              System.out.print(a[j][i]+ "  ");
+            }
+        System.out.println();
+      }
+    }
+}
+   
+  
